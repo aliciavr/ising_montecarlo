a193c0352a9347beb03001ae5f9936e3226fdc13
Alicia Vázquez Ramos <aliciavr@ugr.es>
2023-04-21 17:52:57 +0200

    Thermalization and reverse temperature computing

--- main.cpp
+++ main.cpp
@@ -10,9 +10,11 @@
 static const double Tc = 2 / std::log(1 + std::sqrt(2));
 static const double NUM_T = 27;
 double TEMPERATURES[] = {1.0, 1.1,1.2, 1.3,1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.15,2.2, 2.25,2.3, 2.35, 2.4, 2.45,2.5,2.6, 2.7, 2.8, 2.9, 3.0, 3.2, 3.4};
-static const double NUM_L = 4;
+static const double NUM_L = 5;
 double L_VALUES[] = { 10, 20, 40,  80, 100};
-const int NUM_MC = 250000;
+const int NUM_MC = 50000;
+const int NUM_TERM = NUM_MC * 0.2;
+const int M = NUM_MC - NUM_TERM;
 
 void create_graph_err(std::vector<double> x_values, std::vector<double> y_values, std::vector<double> err_values,
                       char* title, char* x_label, char* y_label, char* filename) {
@@ -84,8 +86,8 @@ void print_v(double* v, int n) {
 
 void print_std_v(std::vector<double> v) {
     std::cout << "[";
-    for (double i: v) {
-        std::cout << i << ", ";
+    for(int i = v.size() - 1; i >= 0; i--) {
+        std::cout << v[i] << ", ";
     }
     std::cout << "\n";
 }
@@ -93,14 +95,6 @@ void print_std_v(std::vector<double> v) {
 double get_var_m(double avg_m2, double avg_m) {
     double var = avg_m2 - std::pow(avg_m, 2);
     return var;
-}
-
-double onsager(double T) {
-    double os = 0.0;
-    if (T <= Tc) {
-        os = std::pow(1 - std::pow(std::sinh(2/T), -4), 1/8);
-    }
-    return os;
 }
 
 void unordered_initialize_s(int** s, double L, std::mt19937 GEN, std::uniform_real_distribution<> uniform) {
@@ -121,7 +115,6 @@ double binder_ratio(double avg_m4, double avg_m2) {
 }
 
 double magnetic_susceptibility(double N, double T, double var_m) {
-    //double var = avg_m2 - std::pow(avg_m, 2);
     double ms = N / T * var_m; // k considered k = 1
     return ms;
 }
@@ -131,7 +124,7 @@ double magnetization_error(double var_m, double c) {
     if (c != 1.0) {
         tau_m = c / (1.0 - c);
     }
-    double m_err = std::sqrt(var_m * (2 * tau_m + 1) / NUM_MC);
+    double m_err = std::sqrt(var_m * (2 * tau_m + 1) / M);
     return m_err;
 }
 
@@ -151,7 +144,7 @@ void ising_metropolis(int** s, int L, int N,  double T, double* avg_m, double* a
 
     double m_aux = 1.0;
     for (int mc = 0; mc < NUM_MC; mc++) {
-        // Montecarlo Step L*L
+        // Monte Carlo Step L*L
         for (int r = 0; r < N; r++) {
             int n = select_point_uniform(GEN);
             int m = select_point_uniform(GEN);
@@ -167,17 +160,20 @@ void ising_metropolis(int** s, int L, int N,  double T, double* avg_m, double* a
             }
         }
 
-        double m =  std::abs(magnetization(s, L, N));
-        *avg_m += m;
-        *avg_m2 += std::pow(m, 2);
-        *avg_m4 += std::pow(m, 4);
+        if (mc > NUM_TERM) {
+            double m =  std::abs(magnetization(s, L, N));
+            *avg_m += m;
+            *avg_m2 += std::pow(m, 2);
+            *avg_m4 += std::pow(m, 4);
 
-        *c = *c + m * m_aux;
-        m_aux = m;
+            *c = *c + m * m_aux;
+            m_aux = m;
+        }
+
     }
-    *avg_m /= NUM_MC;
-    *avg_m2 /= NUM_MC;
-    *avg_m4 /= NUM_MC;
+    *avg_m /= M;
+    *avg_m2 /= M;
+    *avg_m4 /= M;
 }
 
 int main() {
@@ -201,13 +197,14 @@ int main() {
 
         // Declaring variables.
         std::vector<double> temperatures;
-        std::vector<double> onsager_values;
         std::vector<double> avg_m_values;
         std::vector<double> avg_m_err_values;
         std::vector<double> ms_values;
         std::vector<double> binder_values;
 
-        for (int t = 0; t < NUM_T; t++) {
+        unordered_initialize_s(s, L, GEN, uniform);
+
+        for (int t = NUM_T - 1; t >= 0; t--) {
             const double T = TEMPERATURES[t];
             std::cout << "Computing T = " << T << std::endl;
 
@@ -216,14 +213,13 @@ int main() {
             double avg_m4 = 0.0;
             double c = 0.0;
 
-            unordered_initialize_s(s, L, GEN, uniform);
             ising_metropolis(s, L, N, T, &avg_m, &avg_m2, &avg_m4, &c, GEN, uniform, select_point_uniform);
 
             // Auxiliary calculations.
             double var_m = get_var_m(avg_m2, avg_m);
 
             // Compute the average magnetization error.
-            c = (c / NUM_MC - std::pow(avg_m, 2)) / var_m;
+            c = (c / M - std::pow(avg_m, 2)) / var_m;
             double avg_m_err = magnetization_error(var_m, c);
 
             // Compute the magnetic susceptibility value.
@@ -238,7 +234,6 @@ int main() {
 
             // Store results.
             temperatures.push_back(T);
-            onsager_values.push_back(onsager(T));
             avg_m_values.push_back(avg_m);
             avg_m_err_values.push_back(avg_m_err);
             ms_values.push_back(ms);
@@ -249,8 +244,6 @@ int main() {
         std::cout << "L = " << L << ", N = " << N << ", NUM_MC = " << NUM_MC << ", NUM_T = " << NUM_T << std::endl;
         std::cout << "#Temperatures: " << std::endl;
         print_std_v(temperatures);
-        std::cout << "#Onsager: " << std::endl;
-        print_std_v(onsager_values);
         std::cout << "#Average Magnetization: " << std::endl;
         print_std_v(avg_m_values);
         std::cout << "#Average Magnetization Errors: " << std::endl;
